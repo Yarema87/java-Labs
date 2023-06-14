a6bb254ef78b9795faef74f0412fcd9edfe49b36
Yarema87 <gekon4uk@gmail.com>
2023-06-14 13:38:53 +0300

    added second lab

---
+++ src/main/java/ua/lviv/iot/algo/part1/lab1/models/Chainsaw.java
@@ -0,0 +1,38 @@
+package ua.lviv.iot.algo.part1.lab1.models;
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+@ToString
+
+public class Chainsaw extends Saw{
+    private double fuelTankCapacity;
+    private double fuelLevel;
+    public boolean start() {
+        setWorking(true);
+        return getWorking();
+    }
+    public boolean stop() {
+        setWorking(false);
+        return getWorking();
+    }
+    public double charge(double fuel){
+        this.fuelLevel = fuelLevel + fuel;
+        return this.fuelLevel;
+    }
+    public String convertingToString(){
+        Chainsaw chainsaw = new Chainsaw();
+        return chainsaw + "";
+    }
+    @Override
+    public double getRemainingWorkTime() {
+        return (fuelLevel / 0.6);
+    }
+    public Chainsaw(String brand, int power, boolean isWorking, double workingTimeInHours, double fuelTankCapacity, double fuelLevel){
+        super(brand, power, isWorking, workingTimeInHours);
+        this.fuelTankCapacity = fuelTankCapacity;
+        this.fuelLevel = fuelLevel;
+    }
+}
